13d61584b8f82bc9b320db8cbca160de4240c919
hellomatia <shuaigejp80@gmail.com>
2023-12-14 15:14:06 +0900

    init

---
+++ live-streaming/src/main/java/com/startv/livestreaming/LiveStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.startv.livestreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveStreamingApplication.class, args);
+	}
+
+}
---
+++ live-streaming/src/test/java/com/startv/livestreaming/LiveStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startv.livestreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
